3f4fa728b37aeee466709d4dd7a494c4e39288b5
mastu <vikto132@gmail.com>
2022-04-15 23:39:51 +0700

    lol

--- src/app/pages/auth-module/components/login/login.component.ts
+++ src/app/pages/auth-module/components/login/login.component.ts
@@ -89,11 +89,12 @@ export class LoginComponent implements OnInit {
     this.helperService.markFormGroupTouched(this.registerForm);
     if (this.registerForm.invalid) return;
     // this.helperService.showFullLoading();
-    const { usr, pwd } = this.registerForm.value;
+    const { usr, pwd, name } = this.registerForm.value;
     this.loading = true;
     this.apiService.register({
-      username: usr,
-      password: pwd
+      username: name,
+      password: pwd,
+      email: usr
     }).subscribe((res) => {
       this.helperService.hideFullLoading();
       this.loading = false;
